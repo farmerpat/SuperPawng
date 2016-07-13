510f16764eecde29136f3319be4b30ee14a6ee7a
Patrick Connelly <patricksparbanieconnelly@gmail.com>
2016-07-13 00:22:06 -0400

    use balls RigidBody2D velocity component for movement. rand sound vol

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,25 +6,22 @@ public class BallController : MonoBehaviour {
 	private Vector2 movementForce;
 	private AudioSource audioSource;
 	private float collisionTolerance = 0.05f;
+	private float volLowRange = 0.5f;
+	private float volHighRange = 1.0f;
 
 	public float speed;
-	//private AudioClip ballBeepOne;
 
-	// Use this for initialization
 	void Start () {
 		body = GetComponent<Rigidbody2D> ();
-		movementForce = new Vector2 (-1.0f, -0.5f);
+		movementForce = new Vector2 (-0.5f, -0.25f);
 		audioSource = GetComponent<AudioSource> ();
-		//ballBeepOne = audioSource.clip;
 
 	}
 	
 	// when dealing with a RigidBody, FixedUpdate should
 	// be used instead of Update
 	void FixedUpdate () {
-		// USING TRANSLATE IS PROBABLY THE WINNING MOVE FOR PONG PHYSICS
-		//body.AddForce(movementForce * speed);
-		transform.Translate(movementForce * speed * Time.deltaTime); 
+		body.velocity = movementForce * speed;
 
 	}
 
@@ -38,18 +35,9 @@ public class BallController : MonoBehaviour {
 			movementForce.x = 0.0f;
 			movementForce.y = 0.0f;
 
-			//xComponent *= -1.0f;
-			//yComponent *= -1.0f;
-
-
-			//movementForce.x = xComponent;
-			//movementForce.y = yComponent;
-
-
 		} else if (collision.gameObject.CompareTag("BackgroundRight")) {
 			Debug.Log ("Player score");
-			//movementForce.x = 0.0f;
-			//movementForce.y = 0.0f;
+
 			xComponent *= -1.0f;
 			movementForce.x = xComponent;
 
@@ -63,32 +51,18 @@ public class BallController : MonoBehaviour {
 
 		} else if (collision.gameObject.CompareTag ("Player")) {
 			// randomize the second param so it doesnt sound so repetetive
-			audioSource.PlayOneShot (audioSource.clip, 1f);
+			float vol = Random.Range(volLowRange, volHighRange);
+			audioSource.PlayOneShot (audioSource.clip, vol);
 
-			//Collider2D collider = collision.collider;
 			ContactPoint2D cp = collision.contacts[0];
-			//Debug.Log ("collision contacts[0]:");
-			//Debug.Log (cp);
-
 			Collider2D collider = collision.collider;
-
 			Vector2 cpp = collision.contacts[0].point;
-			//Debug.Log ("contacts[0].point");
-			//Debug.Log (cpp);
-
 			Vector2 hitBoxCenter = collider.bounds.center;
-			//Debug.Log ("hitBoxCenter:");
-			//Debug.Log (hitBoxCenter);
 
 			float hitBoxTop = collider.bounds.max.y;
 			float hitBoxBottom = collider.bounds.min.y;
 			float hitBoxLeft = collider.bounds.min.x;
 			float hitBoxRight = collider.bounds.max.x;
-
-			// if cpp.y == hitBoxTop, it hit the top
-			// if cpp.y == hitBoxBottom, it hit the bottom
-			// if cpp.x == hitBoxLeft, it hit the back
-			// if cpp.x == hitBoxRight, it hit the front
 
 			// we should make this abs and comp a fn to clean it up
 			if (Mathf.Abs(cpp.y - hitBoxTop) < collisionTolerance) {
@@ -107,21 +81,10 @@ public class BallController : MonoBehaviour {
 				Debug.Log ("i r hit front");
 			}
 
-
-			// see if it hit the top or bottom or front or back
-
-			//body = GetComponent<Rigidbody2D> ();
-
-			// we want to maintain y velocity and reverse the x 
-			//float xComponent = movementForce.x;
-			//float yComponent = movementForce.y;
-
 			// want the result:
 			// 	to have a magnitude that is affected by the
 			// velocity of the paddle
-
-
-
+			//
 			// reverse the directions
 			xComponent *= -1.0f;
 			//yComponent *= -1.0f;
