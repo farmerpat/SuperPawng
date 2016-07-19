6cf56371cc375f9eec56b386c4450db5903cbbad
Patrick Connelly <patricksparbanieconnelly@gmail.com>
2016-07-18 23:24:45 -0400

    try to smooth enemy's movement a bit

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 	public float speed = 5.0f;
+	public float goalPositionTolerance = 0.3f;
+
 	private float ballLastXPos;
 	private float ballLastYPos;
 	private float ballCurrentXPos;
@@ -43,42 +45,24 @@ public class EnemyController : MonoBehaviour {
 			this.ballCurrentXPos = ball.transform.position.x;
 			this.ballCurrentYPos = ball.transform.position.y;
 
-			// calculate our predicted intersection spot and move towards it
-			// we know two points of line that make up the ball's current
-			// path.
-			//
-			// we know the x position that we want the ball to intersect with
-			// 	(the left side of this objects rigidbody2d)
-			//
-			// so we need to find the corresponding y position and begin moving towards it
-			//
 			// there may be ways to optimize this somehow
 			//
 			// when we try to make the AI stronger, we can adjust the paddle's y position
 			// to strike in different ways
-			// 
+			// we could do this by randomly selecting which postion the paddle is to try
+			// and hit the ball with (top middle bottom).
+			//
 			// we can also just increase the speed at which the enemy paddle moves
 
 
 			// get the slope
 			float m = (this.ballCurrentYPos - this.ballLastYPos) / (this.ballCurrentXPos - this.ballLastXPos);
-
-			// we can use point-slope form
-			// y1 - y2 = m(x1 - x2)
-			// y1 = m(x1 - x2) + y2 
-			// y1 - m(x1 - x2) = y2
-			// y2 = y1 - m(x1 - x2)
-			// where 
-			// should give us our desired y position.
-			// if that y position is beyond the range of motion of enemy's paddle,
-			// I think that the ball will hit a wall first.
-			// if that's true, then if m is positive, it will be hitting the top first
-			// and if m is negative, it will be hitting the bottom first
-
 			float desiredYPos = this.ballCurrentYPos - (m * (this.ballCurrentXPos - this.permenantXPos));
 			float currentYPos = transform.position.y;
 
-			if (currentYPos != desiredYPos) {
+			// a better way to slow this down might be do decrease the magnitude of the value
+			// we set movementForce.y to here after it gets pretty close to its destination
+			if (Mathf.Abs(currentYPos - desiredYPos)  > goalPositionTolerance) {
 				// move it closer by changing movementForce.y
 				if (currentYPos < desiredYPos) {
 					movementForce.y = 1.0f;
