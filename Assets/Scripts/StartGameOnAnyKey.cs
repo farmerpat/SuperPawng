569cd13857bf562b8cfa943b52132b684a625e40
Patrick Connelly <patricksparbanieconnelly@gmail.com>
2016-07-20 19:26:08 -0400

    add start on any key fn to title screen

---
+++ Assets/Scripts/StartGameOnAnyKey.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StartGameOnAnyKey : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			SceneManager.LoadScene ("Level1");
+
+		}
+	}
+}
