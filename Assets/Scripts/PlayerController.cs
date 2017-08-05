252990b441e518458db1c12c5bd55991fca2ca2d
Patrick Connelly <patricksparbanieconnelly@gmail.com>
2017-08-05 10:58:21 -0400

    prefabs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,21 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
     private Rigidbody2D body;
     public float speed;
+	private float moveVertical = 0.0f;
 
     // Use this for initialization
     void Start () {
         body = GetComponent<Rigidbody2D> ();
     }
 
+	void Update () {
+        moveVertical = Input.GetAxis ("Vertical");
+	}
+
     // Update is called once per frame
     // but FixedUpdate is called immediately before each physics update
     // so we generally want to perform physics-related tasks here
     void FixedUpdate () {
-        float moveVertical = Input.GetAxis ("Vertical");
         Vector2 move = new Vector2 (0, moveVertical);
         body.AddForce (move * speed);
 
